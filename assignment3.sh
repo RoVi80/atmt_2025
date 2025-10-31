9b67b258f0208c3eaa23c47257818a5ce3609e2d
Rolando Villaseñor <101873758+RoVi80@users.noreply.github.com>
2025-10-31 16:30:08 +0100

    Update assignment3.sh
    deleted the ignore_checkpoints

--- assignment3.sh
+++ assignment3.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=16GB
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=1
-#SBATCH --output=out_assignment3_joint_bpe.out
+#SBATCH --output=out_assignment3_joint_bpe_resume.out
 
 module load gpu
 module load mamba
@@ -13,18 +13,18 @@ source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
 # PREPARE DATA WITH JOINT BPE
-python preprocess.py \
-    --source-lang cz \
-    --target-lang en \
-    --raw-data ~/shares/cz-en/data/raw \
-    --dest-dir ./cz-en/data/prepared_joint_bpe \
-    --model-dir ./cz-en/tokenizers_joint_bpe \
-    --test-prefix test \
-    --train-prefix train \
-    --valid-prefix valid \
-    --joint-bpe \
-    --joint-vocab-size 16000 \
-    --force-train
+# python preprocess.py \
+#    --source-lang cz \
+#    --target-lang en \
+#    --raw-data ~/shares/cz-en/data/raw \
+#    --dest-dir ./cz-en/data/prepared_joint_bpe \
+#    --model-dir ./cz-en/tokenizers_joint_bpe \
+#    --test-prefix test \
+#    --train-prefix train \
+#    --valid-prefix valid \
+#    --joint-bpe \
+#    --joint-vocab-size 16000 \
+#    --force-train
 
 # TRAIN WITH JOINT BPE
 python train.py \
@@ -39,7 +39,6 @@ python train.py \
     --max-epoch 7 \
     --log-file cz-en/logs_joint_bpe/train_joint_bpe.log \
     --save-dir cz-en/checkpoints_joint_bpe/ \
-    --ignore-checkpoints \
     --encoder-dropout 0.1 \
     --decoder-dropout 0.1 \
     --dim-embedding 256 \
