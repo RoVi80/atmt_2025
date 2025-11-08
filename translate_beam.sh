23bc68739f727f689d0b1e47bc96b1a05237989a
Rolando Villaseñor <101873758+RoVi80@users.noreply.github.com>
2025-11-08 23:44:57 +0100

    Update translate_beam.sh
    back to og code

--- translate_beam.sh
+++ translate_beam.sh
@@ -9,19 +9,14 @@ module load gpu
 module load mamba
 source activate atmt
 
-# Take just 10 sentences for testing
-head -10 ~/shares/cz-en/data/raw/test.cz > ~/test_small.cz
-head -10 ~/shares/cz-en/data/raw/test.en > ~/test_small.en
-
-# Run beam search on small sample
 python translate_beam.py \
     --cuda \
-    --input ~/test_small.cz \
+    --input ~/shares/cz-en/data/raw/test.cz \
     --src-tokenizer cz-en/tokenizers_joint_bpe/cz-en-joint-bpe-16000.model \
     --tgt-tokenizer cz-en/tokenizers_joint_bpe/cz-en-joint-bpe-16000.model \
     --checkpoint-path cz-en/checkpoints_joint_bpe_v2/checkpoint_best.pt \
-    --output ~/test_beam_small.txt \
+    --output cz-en/output_beam5.txt \
     --beam-size 5 \
     --max-len 300 \
     --bleu \
-    --reference ~/test_small.en
+    --reference ~/shares/cz-en/data/raw/test.en
