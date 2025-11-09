3a34405e3b91d563254983ba733fd3e1a02be025
Rolando Villaseñor <101873758+RoVi80@users.noreply.github.com>
2025-11-09 09:02:37 +0100

    Update translate_beam.sh
    just test, change afterwards

--- translate_beam.sh
+++ translate_beam.sh
@@ -3,20 +3,23 @@
 #SBATCH --time=23:0:0
 #SBATCH --gpus=1
 #SBATCH --mem=16GB
-#SBATCH --output=out_beam_search.out
+#SBATCH --output=out_beam_search_testsmall.out
 
 module load gpu
 module load mamba
 source activate atmt
 
+head -10 ~/shares/cz-en/data/raw/test.cz > ~/test_small.cz
+head -10 ~/shares/cz-en/data/raw/test.en > ~/test_small.en
+
 python translate_beam.py \
     --cuda \
-    --input ~/shares/cz-en/data/raw/test.cz \
+    --input ~/test_small.cz \
     --src-tokenizer cz-en/tokenizers_joint_bpe/cz-en-joint-bpe-16000.model \
     --tgt-tokenizer cz-en/tokenizers_joint_bpe/cz-en-joint-bpe-16000.model \
     --checkpoint-path cz-en/checkpoints_joint_bpe_v2/checkpoint_best.pt \
-    --output cz-en/output_beam5.txt \
+    --output ~/test_beam_small.txt \
     --beam-size 5 \
     --max-len 300 \
     --bleu \
-    --reference ~/shares/cz-en/data/raw/test.en
+    --reference ~/test_small.en
