2955c0d49648d09313c846d671e8fdd9220156f1
Rolando Villaseñor <101873758+RoVi80@users.noreply.github.com>
2025-12-06 22:34:14 +0100

    Create run_assignment5_timing.sh
    File to run changes in decode.py

---
+++ run_assignment5_timing.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash -l
+#SBATCH --partition teaching
+#SBATCH --time=2:0:0
+#SBATCH --ntasks=1
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --output=out_assignment5_timing.out
+
+module load gpu
+module load mamba
+source activate atmt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
+
+echo "===== Beam size 1 (greedy) ====="
+time python translate.py \
+    --cuda \
+    --input ~/shares/cz-en/data/raw/test.cz \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
+    --output cz-en/output_k1.txt \
+    --max-len 300 \
+    --beam-size 1
+
+echo "===== Beam size 3 ====="
+time python translate.py \
+    --cuda \
+    --input ~/shares/cz-en/data/raw/test.cz \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
+    --output cz-en/output_k3.txt \
+    --max-len 300 \
+    --beam-size 3
+
+echo "===== Beam size 5 ====="
+time python translate.py \
+    --cuda \
+    --input ~/shares/cz-en/data/raw/test.cz \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
+    --output cz-en/output_k5.txt \
+    --max-len 300 \
+    --beam-size 5
